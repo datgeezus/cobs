ecd8fa71e07c65887d128b35fe3fa6a7095ff800
Jesus Sanchez <jesus.san.sev@gmail.com>
2014-03-08 11:49:35 -0600

    initial commit

---
+++ cobs.h
@@ -0,0 +1,13 @@
+// Based on https://github.com/jacquesf/COBS-Consistent-Overhead-Byte-Stuffing/blob/master/cobs.h
+
+#ifndef COBS_H
+#define	COBS_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+size_t cobs_encode(const uint8_t * input, size_t length, uint8_t * output);
+size_t cobs_decode(const uint8_t * input, size_t length, uint8_t * output);
+
+#endif	/* COBS_H */
+

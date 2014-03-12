639559800e644f9458b7d24202a01d84d0740188
Jesus Sanchez <jesus.san.sev@gmail.com>
2014-03-12 17:58:08 -0600

    modified to use pointers

--- cobs.c
+++ cobs.c
@@ -1,46 +1,47 @@
+// http://conferences.sigcomm.org/sigcomm/1997/papers/p062.pdf
 #include "cobs/cobs.h"
-#include <stdint.h>
-#include <stddef.h>
+
 
 /* Stuffs "length" bytes of data at the location pointed to by
  * "input", writing the output to the location pointed to by
  * "output". Returns the number of bytes written to "output".
- *
- * Remove the "restrict" qualifiers if compiling with a
- * pre-C99 C dialect.
  */
-size_t cobs_encode(const uint8_t * input, size_t length, uint8_t * output)
-{
-    size_t read_index = 0;
-    size_t write_index = 1;
-    size_t code_index = 0;
-    uint8_t code = 1;
 
-    while(read_index < length)
+
+
+void cobs_encode(const uint8_t* input, size_t length, uint8_t* output)
+{
+    const uint8_t* end = input + length;    // end of input buffer
+    uint8_t* codeBytePos = output;          // holds pointer to the position of a code byte
+    uint8_t codeByte = 1;                   // code byte
+
+    /* first byte of the input buffer is in the second position of output buffer */
+    ++output;
+
+    /* iterate over the input bytes */
+    while (input < end)
     {
-        if(input[read_index] == 0)
+        if (*input == 0)
         {
-            output[code_index] = code;
-            code = 1;
-            code_index = write_index++;
-            read_index++;
+            *codeBytePos = codeByte;        // put the code byte in the corresponding position
+            codeByte = 1;                   // reset the code count
+            codeBytePos = output;           // update the code byte position
         }
         else
         {
-            output[write_index++] = input[read_index++];
-            code++;
-            if(code == 0xFF)
+            *output = *input;               // put the
+            ++codeByte;                     //
+            if (codeByte == 0xFF)
             {
-                output[code_index] = code;
-                code = 1;
-                code_index = write_index++;
+                *codeBytePos = codeByte;
+                codeByte = 1;
+                codeBytePos = output;
             }
         }
+        ++output;
+        ++input;
     }
-
-    output[code_index] = code;
-
-    return write_index;
+    *codeBytePos = codeByte;
 }
 
 /* Unstuffs "length" bytes of data at the location pointed to by
@@ -48,16 +49,12 @@ size_t cobs_encode(const uint8_t * input, size_t length, uint8_t * output)
  * "output". Returns the number of bytes written to "output" if
  * "input" was successfully unstuffed, and 0 if there was an
  * error unstuffing "input".
- *
- * Remove the "restrict" qualifiers if compiling with a
- * pre-C99 C dialect.
  */
-size_t cobs_decode(const uint8_t * input, size_t length, uint8_t * output)
+size_t cobs_decode(const uint8_t* input, size_t length, uint8_t* output)
 {
     size_t read_index = 0;
     size_t write_index = 0;
     uint8_t code;
-    uint8_t i;
 
     while(read_index < length)
     {
@@ -70,10 +67,12 @@ size_t cobs_decode(const uint8_t * input, size_t length, uint8_t * output)
 
         read_index++;
 
+        uint8_t i;
         for(i = 1; i < code; i++)
         {
             output[write_index++] = input[read_index++];
         }
+
         if(code != 0xFF && read_index != length)
         {
             output[write_index++] = '\0';
--- cobs.h
+++ cobs.h
@@ -6,8 +6,8 @@
 #include <stdint.h>
 #include <stddef.h>
 
-size_t cobs_encode(const uint8_t * input, size_t length, uint8_t * output);
-size_t cobs_decode(const uint8_t * input, size_t length, uint8_t * output);
+void cobs_encode(const uint8_t* input, size_t length, uint8_t* output);
+size_t cobs_decode(const uint8_t* input, size_t length, uint8_t* output);
 
 #endif	/* COBS_H */
 
